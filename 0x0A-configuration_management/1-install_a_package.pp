4514f948cad448f268eb0282ddb54625c2ea8599
Sammy-OD <odunlamisamuel4@gmail.com>
2023-09-23 08:13:03 +0100

    quick edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,8 +1,13 @@
 # install flask -v 2.1.0
 
+package { 'python3-pip':
+  ensure => installed,
+}
+
 exec { 'install_flask':
   command     => '/usr/bin/pip3 install flask==2.1.0',
   path        => ['/usr/bin'],
-  refreshonly => true,
-  subscribe   => Package['python3-pip'],
+  creates     => '/usr/local/lib/python3.X/dist-packages/flask', # Adjust the path according to your Python version
+  require     => Package['python3-pip'],
+  logoutput   => true,
 }
