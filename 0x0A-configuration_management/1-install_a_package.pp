a73c676e6063abe5b0733dfffe6ff2b696d5b802
Sammy-OD <odunlamisamuel4@gmail.com>
2023-09-23 08:10:26 +0100

    quick edit

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,8 @@
-# install puppet-lint -v 2.5.0
+# install flask -v 2.1.0
 
-exec { 'puppet-lint':
-  command => '/usr/bin/apt-get -y install puppet-lint -v 2.5.0',
+exec { 'install_flask':
+  command     => '/usr/bin/pip3 install flask==2.1.0',
+  path        => ['/usr/bin'],
+  refreshonly => true,
+  subscribe   => Package['python3-pip'],
 }
